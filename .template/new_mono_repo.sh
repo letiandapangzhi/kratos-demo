73f92a09770c952d6b87541ce29f98316deb595f
wurunxin <wurunxin@xiaotucc.com>
2025-07-02 16:51:03 +0800

    mono模板加入zap日志

--- .template/new_mono_repo.sh
+++ .template/new_mono_repo.sh
@@ -15,7 +15,7 @@ echo "创建服务 $service_name ..."
 
 current_dir=$(pwd)
 
-echo "复制api模板并修改名称..."Add commentMore actions
+echo "复制api模板并修改名称..."
 cp -r "$current_dir/.template/api/layout" "$current_dir/api/$service_name"
 find "$current_dir/api/$service_name" -name "*.proto" | xargs sed -i '' "s/layout/$service_name/g"
 
@@ -26,7 +26,7 @@ mv "$current_dir/app/$service_name/cmd/layout" "$current_dir/app/$service_name/c
 # 替换conf的proto路径
 find "$current_dir/app/$service_name/internal/conf" -name "*.proto" | xargs sed -i '' "s/layout/$service_name/g"
 # 遍历所有.go文件（排除wire_gen.go和*pb.go）
-find "$current_dir/app/$service_name" -name "*.go" ! -name "wire_gen.go" ! -name "*pb.go" -type f | while read file; do
+find "$current_dir/app/$service_name" -name "*.go" -o -name "*.yaml" ! -name "wire_gen.go" ! -name "*pb.go" -type f | while read file; do
     # echo "处理文件: $file"
     sed -i "" "s/layout/$service_name/g" "$file"
 done
